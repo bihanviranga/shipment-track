946290e6c1a4f81f62d271813b8142959f2e64b1
bihanviranga <bihanviranga@gmail.com>
2024-10-29 02:42:49 +0530

    chore(server): initial commit

---
+++ server/src/router.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+export const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log("/ PATH");
+  res.json({ message: "hello" });
+});
+
+router.get("/ping", (req, res) => {
+  console.log("/ping PATH");
+  res.json({ ping: "pong" });
+});
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import "dotenv/config";
+import { router } from "@/router";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
